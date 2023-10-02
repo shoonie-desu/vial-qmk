aec7d0de4b384175a93f55cf6b286c8e4b11f7b5
Syuny <shoonieshoe@gmail.com>
2023-10-02 15:28:26 +0900

    zs65 i5v2

---
+++ keyboards/shoonie/zensai_65/i5v2/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x28, 0x5E, 0xA7, 0x11, 0x24, 0x84, 0x41, 0xFE}
+
+#define VIAL_UNLOCK_COMBO_ROWS {1, 2}
+#define VIAL_UNLOCK_COMBO_COLS {13, 13}
