048187f8f29b10bc54b1a00c54fba69424c92699
shoonie-desu <106895098+shoonie-desu@users.noreply.github.com>
2026-06-08 08:53:02 +0900

    c8v4 init

---
+++ keyboards/shoonie/zensai_chiffre/c8v4/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xB1, 0xF8, 0xB7, 0x7B, 0x90, 0x56, 0xCE, 0x2E}
+
+#define VIAL_UNLOCK_COMBO_ROWS {3, 3}
+#define VIAL_UNLOCK_COMBO_COLS {4, 7}
